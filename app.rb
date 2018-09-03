99fa5cb099e3d1c75679a650a928afe9c9cdb3e6
watanabe-koichiro <watanabe-koichiro@KYCNT1703-32.local>
2018-09-03 14:44:58 +0900

    a

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ require 'sinatra/reloader'
 get '/' do
 	'hello world'	
 end
+
+get '/path/to' do
+	"this is [/path/to]"
+end
+
+get '/hello/*' do |name|
+	"hello #{name}. how are you?"
+end
+
+#added
