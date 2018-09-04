755420e3f600494e2816418fc36cc74a0a535182
watanabe-koichiro <watanabe-koichiro@KYCNT1703-32.local>
2018-09-04 15:51:11 +0900

    修正

--- app.rb
+++ app.rb
@@ -16,16 +16,7 @@ get '/' do
 
 	query = %q{SELECT name, created_at, image, good, contents FROM latestgram.article AS ar JOIN latestgram.user AS us ON ar.user_id = us.id}
 	@results = db.query(query)
-=begin
-	results.each do |row|
-		@string += "
-		投稿者名: #{row['name']} <br />
-		投稿日時: #{row['created_at']} <br />
-		いいね #{row['good']} 件<br />
-		#{row['contents']} <br />
-		"
-	end
-=end
+
 	erb :index
 
 end
